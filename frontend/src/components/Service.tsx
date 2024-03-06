57907d960c4c7093eb76380a18e30ed5a05d04e2
Your Name <asmitastha2001@gmail.com>
2024-03-06 22:10:00 +0545

    responsive design added

--- frontend/src/components/Service.tsx
+++ frontend/src/components/Service.tsx
@@ -7,7 +7,7 @@ interface ServiceProps {
 
 const Service: React.FC<ServiceProps> = ({ title, description }) => {
   return (
-    <div className=' bg-gray-950 font-serif mb-5 mr-5 rounded-2xl shadow-stone-900 shadow-2xl'>
+    <div className=' bg-gray-950 hover:bg-stone-800 font-serif mb-5 mr-5 rounded-2xl shadow-stone-900 hover:shadow-stone-950 shadow-2xl'>
       <div className="h-[200px] w-[350px] p-7">
         <h2 className='font-bold text-xl text-blue-400 mb-3 justify-center text-center'>{title}</h2>
         <p className='text-cyan-100 text-center justify-center'>{description}</p>
