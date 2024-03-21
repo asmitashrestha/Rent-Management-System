f8efccf9675a7321673d22d5c53911d7d694bd20
ShirajShrestha <shresthashiraj333@gmail.com>
2024-03-21 13:04:03 +0545

    Test Floor Page

--- frontend/src/components/floors/FloorPage.tsx
+++ frontend/src/components/floors/FloorPage.tsx
@@ -1,13 +1,54 @@
 import CustomerComponent from "../CustomerComponent";
 import EmptyFloorCard from "./EmptyFloorCard";
 import FloorHeader from "./FloorHeader";
-
+import ProxyBIll from "./ProxyBIll";
+import ProxyPayment from "./ProxyPayment";
+import { useModalStore } from "../../stores/ModalStore";
+import RemovePopup from "./RemovePopup";
 const FloorPage = () => {
+  const isOpen = useModalStore((state) => state.isOpen);
+  const closeModal = useModalStore((state) => state.closeModal);
+  const openModal = useModalStore((state) => state.openModal);
+
+  const handleOutsideClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    if (event.target === event.currentTarget) {
+      closeModal();
+    }
+  };
+
   return (
-    <div>
-      <FloorHeader />
-      <EmptyFloorCard />
-      {/* <CustomerComponent /> */}
+    <div className="flex flex-row justify-center bg-background lg:w-3/5 m-auto ">
+      <div className="flex flex-col w-3/5">
+        <FloorHeader />
+        {/* <EmptyFloorCard /> */}
+        <CustomerComponent />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <div className="flex flex-row bg-background font-heading p-2 text-lg gap-4">
+          <button className="bg-accent text-background px-4 py-2 rounded-md w-full">
+            Generate Bill
+          </button>
+          <button
+            className="bg-accent text-background px-4 py-2 rounded-md w-full"
+            onClick={openModal}
+          >
+            Remove Tenant
+          </button>
+        </div>
+        <span className="mt-8">
+          <ProxyBIll />
+        </span>
+        <span className="mt-6">
+          <ProxyPayment />
+        </span>
+      </div>
+      {isOpen && (
+        <div onClick={handleOutsideClick}>
+          <RemovePopup />
+        </div>
+      )}
     </div>
   );
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,7 +10,30 @@ export default {
       colors: {
         "primary-bg": "#E4E3EB",
         "fb-blue": "#0071FF",
-        coffee: "#6F4E37",
+
+        text: "#050316",
+        background: "#fbfbfe",
+        primary: "#2f27ce",
+        secondary: "#dddbff",
+        accent: "#443dff",
+        optional: "#2274A5",
+      },
+      fontSize: {
+        sm: "0.750rem",
+        base: "1rem",
+        xl: "1.333rem",
+        "2xl": "1.777rem",
+        "3xl": "2.369rem",
+        "4xl": "3.158rem",
+        "5xl": "4.210rem",
+      },
+      fontFamily: {
+        heading: "Poppins",
+        body: "Poppins",
+      },
+      fontWeight: {
+        normal: "400",
+        bold: "700",
       },
     },
   },
